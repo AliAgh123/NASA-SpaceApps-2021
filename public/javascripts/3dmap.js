63d558cab9865ad590e7fecbeb9ffc5a49b5faa6
Ali <ali.agh777@gmail.com>
2021-09-30 18:56:42 +0300

    fixed the animation + some edits

---
+++ public/javascripts/3dmap.js
@@ -0,0 +1,82 @@
+mapboxgl.accessToken =
+"pk.eyJ1IjoiYWxpLWFnaCIsImEiOiJja3U0OWJlNW4weDgwMzFvNmhsY2dhMWZ0In0._9bFwcY83fo_G3G2EuI8xw";
+const map = new mapboxgl.Map({
+style: "mapbox://styles/mapbox/light-v10",
+center: [-73.935242, 40.73061],
+zoom: 15.6,
+pitch: 45,
+bearing: -17.6,
+container: "3dmap",
+antialias: true,
+});
+
+
+// add fullscreen button
+map.addControl(new mapboxgl.FullscreenControl());
+
+
+// add navigation control (zoom in, zoom out, direction)
+map.addControl(new mapboxgl.NavigationControl());
+
+
+
+// define the geocoder and it functionality
+const geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl
+    });
+
+
+// add the geocoder to the site
+document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+
+
+// load the 3d building layer
+map.on("load", () => {
+// Insert the layer beneath any symbol layer.
+const layers = map.getStyle().layers;
+const labelLayerId = layers.find(
+    (layer) => layer.type === "symbol" && layer.layout["text-field"]
+).id;
+
+// The 'building' layer in the Mapbox Streets
+// vector tileset contains building height data
+// from OpenStreetMap.
+map.addLayer(
+    {
+        id: "add-3d-buildings",
+        source: "composite",
+        "source-layer": "building",
+        filter: ["==", "extrude", "true"],
+        type: "fill-extrusion",
+        minzoom: 15,
+        paint: {
+            "fill-extrusion-color": "#aaa",
+
+            // Use an 'interpolate' expression to
+            // add a smooth transition effect to
+            // the buildings as the user zooms in.
+            "fill-extrusion-height": [
+                "interpolate",
+                ["linear"],
+                ["zoom"],
+                15,
+                0,
+                15.05,
+                ["get", "height"],
+            ],
+            "fill-extrusion-base": [
+                "interpolate",
+                ["linear"],
+                ["zoom"],
+                15,
+                0,
+                15.05,
+                ["get", "min_height"],
+            ],
+            "fill-extrusion-opacity": 0.6,
+        },
+    },
+    labelLayerId
+);
+});
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,20 @@
-$(".left-nav-togglers").click(function(){
-    $(".left-nav").toggle(500);
+// $(".left-nav-togglers").click(function(){
+//     $(".left-nav").toggle(500);
+// });
+
+$(".slide-right").click(function() {
+    var leftNavStyle = getComputedStyle(document.querySelector(".left-nav"));
+    var opacity = leftNavStyle.opacity;
+    if(opacity == 0){
+        $(".left-nav").toggle();
+        $(".left-nav").animate({opacity: 1});
+    }
+});
+$(".slide-left").click(function() {
+    var leftNavStyle = getComputedStyle(document.querySelector(".left-nav"));
+    var opacity = leftNavStyle.opacity;
+    if (opacity == 1) {
+        $(".left-nav").animate({opacity: 0});
+        setTimeout(() => { $(".left-nav").toggle(); }, 500);
+    }
 });
